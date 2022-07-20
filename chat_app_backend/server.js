2c66ed8ae17795809cd36531ec560cbd8ea8a160
SHAHYASHNILESH <91772554+SHAHYASHNILESH@users.noreply.github.com>
2022-07-20 10:21:52 +0530

    Update server.js

--- chat_app_backend/server.js
+++ chat_app_backend/server.js
@@ -1,13 +1,16 @@
 const express=require('express');
 const cors=require('cors');
 const app=express();
+
 const rooms=['general','tech','finance','crypto'];
+
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 app.use(cors());
 
 const server=require('http').createServer(app);
 const PORT=5001;
+
 const io=require('socket.io')(server,{
     cors:{
         origin:"http://localhost:3000",
